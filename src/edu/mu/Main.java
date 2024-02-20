b431e0728d569dbce0d1b69f299bb49b651a0567
DukeMinty <atorby123@gmail.com>
2024-02-20 15:01:43 -0600

    made initializeStock and displayAll methods

--- src/edu/mu/Main.java
+++ src/edu/mu/Main.java
@@ -3,7 +3,7 @@ package edu.mu;
 public class Main {
 	public static void main(String[] args) {
 		// Instantiate vehicleManager, perform operations based on the requirements.
-		VehicleManager vehicleManager = new VehicleManager();
+//		VehicleManager vehicleManager = new VehicleManager();
 		// Read vehicle data from the vehcileList.csv file and initialize objects.
 		// TODO
 		// Display all vehicle information.
--- src/edu/mu/vehiclemanager/VehicleManager.java
+++ src/edu/mu/vehiclemanager/VehicleManager.java
@@ -1,5 +1,78 @@
 package edu.mu.vehiclemanager;
 
-public class VehicleManager {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import edu.mu.vehicle.FuelType;
+import edu.mu.vehicle.StartMechanism;
+import edu.mu.vehicle.Vehicle;
+import edu.mu.vehicle.VehicleColor;
+import edu.mu.vehicle.types.Car;
+import edu.mu.vehicle.types.MotorBike;
+import edu.mu.vehicle.types.SUV;
+import edu.mu.vehicle.types.Truck;
 
+public class VehicleManager {
+	
+	private final static String vehicleFilePath = "files/vehicleList.csv";
+	private ArrayList<Vehicle> vehicleList;
+	
+	public boolean initializeStock() {
+		try (BufferedReader br = new BufferedReader(new FileReader(vehicleFilePath))) {
+		    String line;
+		    @SuppressWarnings("unused")
+			String headerLine = br.readLine();
+		    while ((line = br.readLine()) != null) {
+		        String[] values = line.split(",");
+		        String type = values[0];
+		        String brand = values[1];
+		        String make = values[2];
+		        long modelYear = Long.parseLong(values[3]);
+		        double price = Double.parseDouble(values[4]);
+		        VehicleColor color = VehicleColor.valueOf(values[5]);
+		        FuelType fuelType = FuelType.valueOf(values[6]);
+				double mileage = Double.parseDouble(values[7]);
+				double mass = Double.parseDouble(values[8]);
+				int cylinders = Integer.parseInt(values[9]);
+				double gasTankCapacity = Double.parseDouble(values[10]);
+				StartMechanism startType = StartMechanism.valueOf(values[11]);
+	            if("Truck".equals(type)) {
+	            	Vehicle vehicle = new Truck(type,brand,make,modelYear,price,color,fuelType,mileage,mass,cylinders,gasTankCapacity,startType);
+	            	vehicleList.add(vehicle);
+	            }
+	            else if("SUV".equals(type)) {
+	            	Vehicle vehicle = new SUV(type,brand,make,modelYear,price,color,fuelType,mileage,mass,cylinders,gasTankCapacity,startType);
+	            	vehicleList.add(vehicle);
+	            }
+	            else if("Car".equals(type)) {
+	            	Vehicle vehicle = new Car(type,brand,make,modelYear,price,color,fuelType,mileage,mass,cylinders,gasTankCapacity,startType);
+	            	vehicleList.add(vehicle);
+	            }
+	            else if("MotorBike".equals(type)) {
+	            	Vehicle vehicle = new MotorBike(type,brand,make,modelYear,price,color,fuelType,mileage,mass,cylinders,gasTankCapacity,startType);
+	            	vehicleList.add(vehicle);
+	            }
+	            else {
+	            	System.out.println("Error");
+	            }
+		    }
+		    br.close();
+		    return true;
+		}catch (IOException e) {
+			System.out.println("Error occured");
+	        e.printStackTrace();
+	        return false;
+	    }
+		catch(IllegalArgumentException e) {
+			System.out.println("Data is incompatible");
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public void displayAllCarInformation() {
+		for(Vehicle vehicle : vehicleList) {
+			System.out.println(vehicle.toString());
+		}
+	}
 }
