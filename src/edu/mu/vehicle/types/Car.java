61e28b3ba4406bc4625161da4d93a287bbcd11a4
DukeMinty <atorby123@gmail.com>
2024-02-20 10:50:45 -0600

    Made starting point for other vehicle types

---
+++ src/edu/mu/vehicle/types/Car.java
@@ -0,0 +1,37 @@
+package edu.mu.vehicle.types;
+
+import edu.mu.vehicle.FuelType;
+import edu.mu.vehicle.StartMechanism;
+import edu.mu.vehicle.Vehicle;
+import edu.mu.vehicle.VehicleColor;
+
+public class Car extends Vehicle{
+
+	public Car(String type, String brand, String make, long modelYear, double price, VehicleColor color, FuelType fuelType,
+			double mileage, double mass, int cylinders, double gasTankCapacity, StartMechanism startType) {
+		super(brand, make, modelYear, price, color, fuelType, mileage, mass, cylinders, gasTankCapacity, startType);
+		//readFromFile should make sure these are Car and pushstart
+		//but this is an extra measure since they're mandatory traits
+		setType("Car");
+		setStartType(StartMechanism.PUSHSTART);
+	}
+
+	@Override
+	public double calculateMaintenanceCost(double distance) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double calculateFuelEfficiency(double distance, double fuelPrice) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void startEngine() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/edu/mu/vehicle/types/SUV.java
@@ -0,0 +1,37 @@
+package edu.mu.vehicle.types;
+
+import edu.mu.vehicle.FuelType;
+import edu.mu.vehicle.StartMechanism;
+import edu.mu.vehicle.Vehicle;
+import edu.mu.vehicle.VehicleColor;
+
+public class SUV extends Vehicle{
+
+	public SUV(String type, String brand, String make, long modelYear, double price, VehicleColor color, FuelType fuelType,
+			double mileage, double mass, int cylinders, double gasTankCapacity, StartMechanism startType) {
+		super(brand, make, modelYear, price, color, fuelType, mileage, mass, cylinders, gasTankCapacity, startType);
+		//readFromFile should make sure these are SUV and pushstart
+		//but this is an extra measure since they're mandatory traits
+		setType("SUV");
+		setStartType(StartMechanism.PUSHSTART);
+	}
+
+	@Override
+	public double calculateMaintenanceCost(double distance) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double calculateFuelEfficiency(double distance, double fuelPrice) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void startEngine() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
