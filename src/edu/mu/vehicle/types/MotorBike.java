ba52bbf64e0b5ec9097cb9499b9e514b0ae09d89
GSpinks <111899656+GSpinks@users.noreply.github.com>
2024-02-24 10:59:42 -0600

    get calculations for fuel efficiency

--- src/edu/mu/vehicle/types/MotorBike.java
+++ src/edu/mu/vehicle/types/MotorBike.java
@@ -23,8 +23,8 @@ public class MotorBike extends Vehicle{
 
 	@Override
 	public double calculateFuelEfficiency(double distance, double fuelPrice) {
-		// TODO Auto-generated method stub
-		return 0;
+		double fuelEfficiency = getCylinders() * getGasTankCapacity() * fuelPrice / distance * 0.001;
+		return fuelEfficiency;
 	}
 
 	@Override
